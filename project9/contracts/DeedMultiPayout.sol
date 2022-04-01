497c3c0cf69982e4e97d57a152b8946dd0f0372f
skiran017 <skiran017@gmail.com>
2022-04-01 19:58:37 +0530

    deed multi payout smart contract and tests
    -withdraw function for multi payout for n intervals
    -tests

---
+++ project9/contracts/DeedMultiPayout.sol
@@ -0,0 +1,38 @@
+pragma solidity ^0.5.0;
+
+contract DeedMultiPayout {
+    address public lawyer;
+    address payable public beneficiary;
+    uint256 public earliest; //unique timestamp || date
+    uint256 public amount; //in WEI
+
+    //helps to save gas by storing variable in code rather than in storage of blockchain; this cannot be modified later
+    uint256 public constant PAYOUTS = 4;
+    uint256 public constant INTERVAL = 1; //time between each payout in seconds (unit by default)
+    uint256 public paidPayouts;
+
+    constructor(
+        address _lawyer,
+        address payable _beneficiary,
+        uint256 fromNow
+    ) public payable {
+        lawyer = _lawyer;
+        beneficiary = _beneficiary;
+        earliest = now + fromNow;
+        amount = msg.value / PAYOUTS;
+    }
+
+    function withdraw() public {
+        require(msg.sender == lawyer, "lawyer only");
+        require(now >= earliest, "too early");
+        require(paidPayouts < PAYOUTS, "no payouts left");
+
+        uint256 eligiblePayouts = 1 + (now - earliest) / INTERVAL;
+        uint256 duePayouts = eligiblePayouts - paidPayouts;
+        duePayouts = duePayouts + paidPayouts > PAYOUTS
+            ? PAYOUTS - paidPayouts
+            : duePayouts;
+        paidPayouts += duePayouts;
+        beneficiary.transfer(duePayouts * amount);
+    }
+}

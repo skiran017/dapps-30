c91a4a6f86fa3a3d78041aa64c05b6fe9ae78e07
skiran017 <skiran017@gmail.com>
2022-04-03 16:10:19 +0530

    frontend setup
    -frontend using react

--- project10/migrations/2_deploy_contracts.js
+++ project10/migrations/2_deploy_contracts.js
@@ -1,13 +1,13 @@
 const Escrow = artifacts.require('Escrow');
 
 module.exports = function (deployer, _network, accounts) {
-  // deployer.deploy(
-  //   Escrow,
-  //   accounts[1], //payer
-  //   accounts[2], //payee
-  //   1000,
-  //   { from: accounts[0] }
-  // );
+  deployer.deploy(
+    Escrow,
+    accounts[1], //payer
+    accounts[2], //payee
+    1000,
+    { from: accounts[0] }
+  );
   //for test
-  deployer.deploy(Escrow, accounts[1], accounts[2], 1000);
+  // deployer.deploy(Escrow, accounts[1], accounts[2], 1000);
 };

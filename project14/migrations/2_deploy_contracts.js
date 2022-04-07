51497c1b17429c2c8c25d677f3182a4daf52a89c
skiran017 <skiran017@gmail.com>
2022-04-08 01:42:59 +0530

    voting smart contract init

---
+++ project14/migrations/2_deploy_contracts.js
@@ -0,0 +1,17 @@
+const Voting = artifacts.require('Voting');
+
+// module.exports = async function(deployer, _network, accounts) {
+//   await deployer.deploy(Voting);
+//   const voting = await Voting.deployed();
+//   await voting.addVoters([accounts[1], accounts[2], accounts[3]]);
+//   await voting.createBallot(
+//     'Who should be President?',
+//     ['Trump', 'Biden', 'Hillary'],
+//     120
+//   );
+// };
+
+//for testing
+module.exports = function (deployer) {
+  deployer.deploy(Voting);
+};
